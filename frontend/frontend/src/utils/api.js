9b8257b0b3ee60c0d79193e6f642c3897a0e6e49
Oseaghe <oseagheebhomielen@gmail.com>
2025-07-18 22:47:38 +0100

    init commit

--- frontend/frontend/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:8080/api/streaks'; // adjust when deployed
-
-export const fetchStreaks = async (usernames) => {
-    const response = await axios.post(API_URL, { usernames: usernames });
-    return response.data;
-};
